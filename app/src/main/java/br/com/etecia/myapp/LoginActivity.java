9b4b874a51731b310fba4d9445e967ed0cb3f530
unknown <lleonardocamargost@gmail.com>
2023-09-14 22:14:05 -0300

    Codigo em java

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,35 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    ImageView imgLogin;
+    Button btnEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        imgLogin=findViewById(R.id.imgLogin);
+        imgLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SplashActivity.class));
+            }
+        });
+        setContentView(R.layout.login_layout);
+        btnEntrar=findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,MenuPrincipalActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -3,12 +3,24 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
-
+ MaterialCardView cardGaleria;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
+        cardGaleria=findViewById(R.id.cardGaleria);
+        cardGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Cliquei em Galeria",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
